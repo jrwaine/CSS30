cf769a3257e1fe6a9695b6ad779accf3895136b7
Waine Oliveira Jr <waine.oliveira.jr@gmail.com>
2022-04-19 19:17:41 -0300

    Updated project 2

---
+++ project2/client.py
@@ -0,0 +1,77 @@
+from typing import Dict, List, Any, Optional, Literal
+import time
+import enum
+
+
+import click
+import colorama
+from colorama import Back, Fore
+
+class States(enum.Enum):
+    HELD = 1
+    WANTED = 2
+    RELEASED = 3
+
+
+class Client:
+    def __init__(self, n_resources: int) -> None:
+        self.n_resources = n_resources
+        self.curr_resources: Dict[int, States] = {i: States.RELEASED for i in range(n_resources)}
+        self.pub_key = None
+
+    def serve(self):
+        ...
+
+    def take_action(self, action: Literal["ASK", "RELEASE"], resource: int):
+        resource_state = self.curr_resources[resource]
+
+        if(action.upper() == "ASK" and resource_state in (States.HELD, States.WANTED)):
+            click.echo(f"Already holding resource {resource}")
+            return
+        elif(action.upper() == "RELEASE" and resource_state == States.RELEASED):
+            click.echo(f"Already released resource {resource}")
+            return
+
+        ...
+
+
+class ClientUI:
+    def __init__(self, c: Client) -> None:
+        self.client = c
+        
+    def draw_state(self):
+        client = self.client
+        print(f"{Fore.YELLOW}Number of resources: {client.n_resources}")
+        print(f"{Fore.YELLOW}Pub server key: {client.pub_key}")
+        print()
+        print(f"{Fore.YELLOW}Current states: " + {k: v.name for k, v in client.curr_resources.items()})
+
+    def draw_options(self):
+        client = self.client
+
+        action = click.prompt("What action you want to take?", type=click.Choice(["ASK", "RELEASE", "NONE"], case_sensitive=False))
+        if(action.upper() == "NONE"):
+            return
+        if(action.upper() == "ASK"):
+            if(all(v in (States.HELD, States.WANTED) for v in client.curr_resources.values())):
+                click.echo("Holding/Asked all resources already. Unable to ask for more")
+                return
+        elif(action.upper() == "RELEASE"):
+            if(all(v in (States.RELEASED) for v in client.curr_resources.values())):
+                click.echo("Released all resources already. Unable to release more")
+                return
+
+        resource = click.prompt("What resource you want to act on?", type=click.Choice([str(i) for i in range(client.n_resources)]))
+        resource = int(resource)
+        client.take_action(action.upper(), resource)
+
+    def draw(self):
+        click.clear()
+
+        print(f"{Fore.YELLOW}------------ CLIENT STATE ------------")
+        self.draw_state()
+        print(f"{Fore.YELLOW}--------------------------------------")
+
+        print(f"{Fore.YELLOW}------------ CLIENT OPTIONS ------------")
+        self.draw_options()
+        print(f"{Fore.YELLOW}----------------------------------------")
---
+++ project2/server.py
@@ -0,0 +1,65 @@
+from typing import Dict, List, Any, Optional
+import time
+
+from .signer import get_key_pair
+
+import click
+import colorama
+from colorama import Back, Fore
+
+class Server:
+    MAX_RESOURCE_TIME_S: int = 30
+
+    def __init__(self, n_resources: int) -> None:
+        self.n_resources = n_resources
+        self.n_clients = 0
+        self.resource_time = Dict[int, Optional[float]] = {i: None for i in range(n_resources)}
+        self.resource_owner: Dict[int, Optional[int]] = {i: None for i in range(n_resources)}
+        self.queue_resources: Dict[int, List[int]] = {i: [] for i in range(n_resources)}
+        self.pub_key, self.priv_key = get_key_pair()
+
+    @property
+    def resources_time_for_timeout(self) -> Dict[int, Optional[float]]:
+        return {i: self.time_for_timeout(i) for i in range(self.n_resources)}
+
+    @property
+    def time_for_timeout(self, resource: int) -> Optional[float]:
+        val = self.resource_time[resource]
+        return None if val is None else round(time.time() - val, 2)
+
+    def serve(self):
+        ...
+
+    def send_token(self, resource: int):
+        ...
+
+    def timeout_token(self):
+        ...
+
+    def token_liberation(self, pid: int, resource: int):
+        ...
+
+    def remote_ask_token(self, resource: int) -> Optional[int]:
+        ...
+
+class ServerUI:
+    def __init__(self, s: Server) -> None:
+        self.server = s
+        
+    def draw_state(self):
+        serv = self.server
+        print(f"{Fore.YELLOW}Number of resources: {serv.n_resources}")
+        print(f"{Fore.YELLOW}Number of clients: {serv.n_clients}")
+        print(f"{Fore.YELLOW}Public key: {serv.pub_key}")
+        print(f"{Fore.YELLOW}Private key: {serv.priv_key}")
+        print()
+        print(f"{Fore.YELLOW}Resources owners: {serv.resource_owner}")
+        print(f"{Fore.YELLOW}Resources timeout: {serv.resources_time_for_timeout}")
+        print(f"{Fore.YELLOW}Resources queues: {serv.queue_resources}")
+
+    def draw(self):
+        click.clear()
+
+        print(f"{Fore.YELLOW}------------ SERVER STATE ------------")
+        self.draw_state()
+        print(f"{Fore.YELLOW}--------------------------------------")
